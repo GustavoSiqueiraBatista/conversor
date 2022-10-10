bd1b4e9abe01dee49958a9afea5c0c9fcc52130a
Gustavo Siqueira <gustavosiqueira123@outlook.com>
2022-10-10 15:45:21 -0300

    Atualizaçao

--- javascript.js
+++ javascript.js
@@ -1,18 +1,48 @@
-    let num = prompt("Deseja Fazer a Conversao em Dolar ou Euro?")
-    let valor = prompt("Qual o Valor?")
+
+    let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL"
+    let result = document.querySelector('.result');
+    let form = document.querySelector('.form');
+    let input = document.querySelector('#valor');
+
+
+    const select = document.querySelector('#select');
+    const indice = select.value
+
+    console.log(indice)
     
 
-    if (num == "euro"){
-        let valorEuro = 5.11
-        let resultadoEuro = valor * valorEuro
-        alert("Valor em Euro: " + resultadoEuro.toFixed(2))
-    }else if(num == ("dolar")) {
-        let valorDolar = 5.19
-        let resultadoDolar = valor * valorDolar
-        alert ("Valor em Dolar: " + resultadoDolar.toFixed(2))
+    async function get () {
+        const response = await fetch(url)
+
+        console.log(response)
+
+        const data = await response.json()
+        console.log(data)
+
+        console.log(data.EURBRL.bid)
+        console.log(data.USDBRL.bid)
+
+            var valueEur = data.EURBRL.bid;
+            var valueUsd = data.USDBRL.bid
+
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            
+            const newValue = input.value
+                console.log(newValue)
+            
+            if (indice == "EUR") {
+                let resultedEur = valueEur * newValue
+                result.innerHTML = resultedEur.toFixed(2)
+            }else if (indice == "USD"){
+                let resultedUsd = valueUsd * newValue
+                result.innerHTML = resultedUsd.toFixed(2)
+            }
+        })
     }
-    
-    
-    
-    
-    
+    get()
+
+        
+
+
